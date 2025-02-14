e86751e828764a89ffb2e61e4ad091c484966993
saunaa <storytimebed@proton.me>
2025-02-14 03:00:06 +0100

    pew pew

---
+++ shot.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import CircleShape
+from constants import SHOT_RADIUS
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "purple", self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+        
+
+    
