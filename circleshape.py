5e83e7930c70696382a5df0ba2a37adea54fe9ed
saunaa <storytimebed@proton.me>
2025-02-14 02:58:43 +0100

    hitbox/shape for all objects

---
+++ circleshape.py
@@ -0,0 +1,25 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def check_collision(self, circle):
+        if self.position.distance_to(circle.position) >= self.radius + circle.radius:
+            return False
+        return True
+    
+
